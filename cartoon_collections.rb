603bf1e01515fd34a396f8a68b6033bdc7117e96
Jonah Offitzer <joffitzer@gmail.com>
2019-10-18 19:00:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,8 +19,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.include?(cheese_types)
-    return cheese_types
-  else
-    return nil
+  array.find do 
 end
